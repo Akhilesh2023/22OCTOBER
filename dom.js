08fc139302763d395333f3e311b445405e20c497
Akhilesh Rana <akhileshrana20@gmail.com>
2024-10-23 17:04:21 +0530

    save changes

---
+++ dom.js
@@ -0,0 +1,12 @@
+// const mainHeading = document.querySelector("#main-heading");
+// console.log(mainHeading);
+const mainHeading1 = document.querySelector("h1");
+console.log(mainHeading1);
+mainHeading1.textContent = "Hello my name is akhilesh Rana";
+mainHeading1.style.backgroundColor = "red";
+mainHeading1.style.color = "yellow";
+mainHeading1.style.fontSize = "100px";
+mainHeading1.style.alignContent = "center";
+
+const contenct = document.querySelector("h1");
+console.log(contenct, innerhtml);
